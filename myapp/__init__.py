ea2f73d08919eac6275f1dcecf8fbbb4daedf002
tangsu99 <18763952906@163.com>
2025-01-14 21:06:02 +0800

    注释测试数据库

--- myapp/__init__.py
+++ myapp/__init__.py
@@ -5,7 +5,7 @@ db = SQLAlchemy()
 
 def create_app():
     app = Flask(__name__)
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///your_database.db'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///your_database.db' # 测试数据库
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     db.init_app(app)
     
