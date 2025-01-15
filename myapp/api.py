99f6522403b02f49d3d3c2628a75c06f020aef98
tangsu99 <18763952906@163.com>
2025-01-15 19:06:16 +0800

    白名单相关

--- myapp/api.py
+++ myapp/api.py
@@ -9,15 +9,18 @@ api = Blueprint('api', __name__)
 
 @api.route('/whitelist/<player>', methods=['GET'])
 def whitelist(player: str):
+    response_data = {'code': 1, 'desc': 'not fond'}
+    result = None
     if len(player) > 16:
         result = Whitelist.query.filter_by(player_uuid=player).first()
-        if result is not None:
-            return jsonify({'code': 0, 'desc': '在白名单中', 'uuid': result.player_uuid, 'name': result.player_name})
     else:
         result = Whitelist.query.filter_by(player_name=player).first()
-        if result is not None:
-            return jsonify({'code': 0, 'desc': '在白名单中', 'uuid': result.player_uuid, 'name': result.player_name})
-    return jsonify({'code': 1, 'desc': 'not fond'})
+    if result is not None:
+        response_data['code'] = 0
+        response_data['desc'] = '在白名单中'
+        response_data['uuid'] = result.player_uuid
+        response_data['name'] = result.player_name
+    return jsonify(response_data)
 
 
 @api.route('/whitelists', methods=['GET'])
