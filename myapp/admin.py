8e454f2952b5fbb45dbeb5770ccbdfcd04fc3cdd
tangsu99 <18763952906@163.com>
2025-02-11 11:32:19 +0800

    admin

--- myapp/admin.py
+++ myapp/admin.py
@@ -1,19 +1,22 @@
 from flask import Blueprint, jsonify, render_template, request
 from flask_login import login_required
-from myapp.db_model import Question, Survey, Option, question_type_map
+from myapp.db_model import Question, Survey, Option, question_type_map, Whitelist
 from myapp import db
+from myapp.utils import required_role
 
 admin = Blueprint('admin', __name__)
 
 
 @admin.route('/')
 @login_required
+@required_role('admin')
 def admin_index():
     return jsonify({'desc': 'admin'})
 
 
 @admin.route('/AllQuestion')
 @login_required
+@required_role('admin')
 def all_question():
     result = Question.query.all()
     data = []
@@ -35,6 +38,8 @@ def all_question():
 
 
 @admin.route("/addSurvey", methods=['POST'])
+@login_required
+@required_role('admin')
 def add_survey():
     survey: Survey = Survey(request.json['name'], request.json['description'])
     db.session.add(survey)
@@ -44,6 +49,8 @@ def add_survey():
 
 
 @admin.route("/addQuestion", methods=['POST'])
+@login_required
+@required_role('admin')
 def add_question():
     req_data = request.json
     question: Question = Question(req_data['survey'], req_data['title'], req_data['score'], req_data['type'])
@@ -55,3 +62,14 @@ def add_question():
         db.session.add(option)
     db.session.commit()
     return jsonify({'code': 0, 'desc': '成功'})
+
+
+@admin.route('/whitelist', methods=['GET'])
+@login_required
+@required_role('admin')
+def whitelists():
+    result = Whitelist.query.all()
+    response_data = {'code': 0, 'desc': 'yes', 'list': []}
+    for i in result:
+        response_data['list'].append({'id': i.id, 'uid': i.user_id, 'name': i.player_name, 'uuid': i.player_uuid})
+    return jsonify(response_data)
