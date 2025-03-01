73a4eb69081badebca5eb609a4e7edd73c60b2ef
tangsu99 <18763952906@163.com>
2025-03-01 20:57:53 +0800

    更改头像的API

--- myapp/user.py
+++ myapp/user.py
@@ -1,5 +1,7 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, request
 from flask_login import login_required, current_user
+
+from myapp import db
 
 user = Blueprint('user', __name__)
 
@@ -36,3 +38,35 @@ def get_whitelist():
             'uuid': item.player_uuid,
         })
     return jsonify(res)
+
+
+@user.route('/setAvatar', methods=['POST'])
+@login_required
+def set_avatar():
+    # 获取请求数据
+    req_data = request.json
+
+    # 检查是否提供了头像 URL
+    if not req_data or 'avatarUrl' not in req_data:
+        return jsonify({
+            'code': 1,
+            'desc': '缺少头像 URL 参数！'
+        }), 400
+
+    avatar_url = req_data['avatarUrl']
+
+    # 修改用户头像
+    try:
+        current_user.avatar = avatar_url
+        db.session.commit()
+        return jsonify({
+            'code': 0,
+            'desc': '头像修改成功！'
+        })
+    except Exception as e:
+        db.session.rollback()
+        return jsonify({
+            'code': 3,
+            'desc': f'头像修改失败：{str(e)}'
+        }), 500
+
