78f4148e83f33faf56839f731e27c659a6c4ef0d
BusyPacket <busypacket@foxmail.com>
2025-03-21 00:13:44 +0800

    添加填空题的自动判分

--- myapp/default.py
+++ myapp/default.py
@@ -1,9 +1,18 @@
 from flask import Blueprint, jsonify, request
 from flask_login import current_user, login_required
+from sqlalchemy import false
 
 from myapp import db
-from myapp.db_model import User, Survey, question_type_map, Response, Whitelist, ResponseDetail, QuestionType, Question, \
-    Option
+from myapp.db_model import (
+    User,
+    Survey,
+    question_type_map,
+    Response,
+    ResponseDetail,
+    QuestionType,
+    Question,
+    Option,
+)
 
 default = Blueprint("default", __name__)
 
@@ -37,9 +46,11 @@ def get_survey(sid: int):
         # 查询题目中的所有选项
         for option in question.options:
             if question.question_type == 3 or question.question_type == 4:
-                question_data["options"].append({"id": option.id, "text": '此处作答'})
+                question_data["options"].append({"id": option.id, "text": "此处作答"})
                 continue
-            question_data["options"].append({"id": option.id, "text": option.option_text})
+            question_data["options"].append(
+                {"id": option.id, "text": option.option_text}
+            )
 
         survey_data["questions"].append(question_data)
 
@@ -53,7 +64,7 @@ def awa(response):
     return None
 
 
-@default.route('/check_survey', methods=['POST'])
+@default.route("/check_survey", methods=["POST"])
 @login_required
 def check_survey():
     user: User = current_user
@@ -61,12 +72,14 @@ def check_survey():
     existing_response = user.responses
     res: Response = awa(existing_response)
     if res is not None:
-        return jsonify({"code": 1, "desc": "您有未完成问卷！", "response": res.survey_id})
+        return jsonify(
+            {"code": 1, "desc": "您有未完成问卷！", "response": res.survey_id}
+        )
 
-    return jsonify({'code': 0, 'desc': '暂无问卷! '})
+    return jsonify({"code": 0, "desc": "暂无问卷! "})
 
 
-@default.route('/start_survey', methods=['POST'])
+@default.route("/start_survey", methods=["POST"])
 @login_required
 def start_survey():
     user: User = current_user
@@ -74,7 +87,9 @@ def start_survey():
     existing_response = user.responses
     res: Response = awa(existing_response)
     if res is not None:
-        return jsonify({"code": 1, "desc": "您有未完成问卷！", "response": res.survey_id})
+        return jsonify(
+            {"code": 1, "desc": "您有未完成问卷！", "response": res.survey_id}
+        )
 
     data = request.get_json()
     type_ = data["playerType"]
@@ -96,14 +111,19 @@ def start_survey():
     db.session.add(new_response)
     db.session.commit()
 
-    return jsonify({
-        "code": 0,
-        "desc": "问卷开始！",
-        "response": new_response.survey_id,
-    }), 201
+    return (
+        jsonify(
+            {
+                "code": 0,
+                "desc": "问卷开始！",
+                "response": new_response.survey_id,
+            }
+        ),
+        201,
+    )
 
 
-@default.route('/complete_survey', methods=['POST'])
+@default.route("/complete_survey", methods=["POST"])
 @login_required
 def complete_survey():
     data = request.get_json()
@@ -117,24 +137,37 @@ def complete_survey():
     count_score = 0
 
     for i in data:
-        question_id = i.get('id')  # 问题ID
+        question_id = i.get("id")  # 问题ID
         answer = i.get("answer")  # 用户答案
+        bingo = False
         if answer is None:
             continue
         question: Question = Question.query.get(question_id)
 
-        # 获取问题的正确选项
-        correct_options = [option.id for option in question.options if option.is_correct]
+        # 获取问题的正确选项,列表元素的值为正确选项的ID
+        correct_options = [
+            option.id for option in question.options if option.is_correct
+        ]
 
         # 处理单选题
-        if question.question_type == 1:  # 单选题
-            if len(answer) == 1 and answer[0] in correct_options:
-                count_score += question.score  # 答案正确，累加分数
-
+        if (
+            question.question_type == 1
+            and len(answer) == 1
+            and answer[0] in correct_options
+        ):
+            bingo = True
         # 处理多选题
-        elif question.question_type == 2:  # 多选题
-            if set(answer) == set(correct_options):  # 用户答案与正确选项完全匹配
-                count_score += question.score  # 答案正确，累加分数
+        elif question.question_type == 2 and set(answer) == set(correct_options):
+            bingo = True
+
+        # 处理填空题
+        elif question.question_type == 3:
+            user_answer: str = answer[0]
+            correct_answer_id = correct_options[0]
+            option: Option = Option.query.get(correct_answer_id)
+            correct_answer: str = option.option_text
+            if user_answer == correct_answer:
+                bingo = True
 
         # 创建答题详情
         if question.question_type == 2:  # 处理多选
@@ -152,6 +185,9 @@ def complete_survey():
                 answer=answer[0],
             )
             db.session.add(new_response_detail)
+
+            if bingo:
+                count_score += question.score
 
     # 标记答卷为已完成
     res.is_completed = True
