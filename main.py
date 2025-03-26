c2f7016f8cbdf3760f957fac6585cc0ca8a60697
tangsu99 <18763952906@163.com>
2025-03-26 09:53:00 +0800

    setup??

--- main.py
+++ main.py
@@ -1,6 +1,23 @@
-from myapp import create_app
+from myapp import create_app, db
+from myapp.db_model import User
 
 app = create_app()
+
+LEN = 50
+
+def setup():
+    print('+' * LEN)
+    print('Setting...')
+    if len(User.query.all()) == 0:
+        username: str = 'root'
+        password: str = '@12345Root'
+        user = User(username, '123456789', 'admin').set_password(password)
+        user.status = 1
+        db.session.add(user)
+        db.session.commit()
+    else:
+        print('Database is not null!')
+    print('+' * LEN)
 
 if __name__ == "__main__":
     app.run(debug=True)
