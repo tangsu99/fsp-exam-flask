d82ddf0ff41f21f45f282f10f677b84dfca4cc6e
tangsu99 <18763952906@163.com>
2025-05-03 06:36:10 +0800

    is_survey_response_expired -> utils

--- myapp/utils.py
+++ myapp/utils.py
@@ -5,6 +5,7 @@ from typing import cast
 from flask import abort, current_app, request
 from flask_login import current_user
 
+from myapp import db
 from myapp.db_model import User, Response
 
 PASSWORD_PATTERN = re.compile(r"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,16}$")
@@ -45,14 +46,14 @@ def check_password(password: str) -> bool:
 
 def is_survey_response_expired(survey_response: Response) -> bool:
     val = current_app.config["VALIDITY_PERIOD"]
-    VALIDITY_PERIOD = timedelta(hours=val) # 有效期为 24h
+    validity_period = timedelta(hours=val) # 有效期为 24h
 
     # 只判断未完成的问卷，已完成的问卷不存在“过期”的说法
     if survey_response.is_completed is False:
         create_time = survey_response.create_time
         create_datetime = create_time.replace(tzinfo=timezone.utc)
         current_datetime = datetime.now(timezone.utc)
-        expired_datetime = create_datetime + VALIDITY_PERIOD
+        expired_datetime = create_datetime + validity_period
         if expired_datetime < current_datetime:
             survey_response.is_completed = True
             survey_response.is_reviewed = 2
