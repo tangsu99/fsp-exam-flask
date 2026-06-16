4675a55228d57cf35bd66a36f9bd6511285924ab
BusyPacket <busypacket@foxmail.com>
2026-06-16 17:18:49 +0800

    style: refactor setup function for improved readability and consistency

--- main.py
+++ main.py
@@ -1,23 +1,27 @@
+from sqlalchemy import func, select
+
 from myapp import create_app, db
-from myapp.db_model import User
+from myapp.db_model import User, UserRole, UserStatus
 
 app = create_app()
 
 LEN = 50
 
+
 def setup():
-    print('+' * LEN)
-    print('Setting...')
-    if db.session.query(User).count() == 0:
+    print("+" * LEN)
+    print("Setting...")
+    if db.session.scalar(select(func.count(User.id))) == 0:
         # Please be sure to change the default password!
-        user = User(username='root', user_qq='123456789', role='admin', status=1)
-        user.password = '@12345Root'
+        user = User(username="root", user_qq="123456789", role=UserRole.ADMIN, status=UserStatus.ACTIVE)
+        user.password = "@12345Root"
         db.session.add(user)
         db.session.commit()
-        print('Created Admin account!')
+        print("Created Admin account!")
     else:
-        print('Database is not null!')
-    print('+' * LEN)
+        print("Database is not null!")
+    print("+" * LEN)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
